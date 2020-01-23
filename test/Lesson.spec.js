c18b84a5d1ff0709b24b084ff1611a1af8374469
larry kirschner <larrykirschner@gmail.com>
2020-01-23 12:28:41 -0800

    lesson tests

--- test/Lesson.spec.js
+++ test/Lesson.spec.js
@@ -12,19 +12,52 @@ describe("Lesson", function() {
     await mongoUnit.drop();
   });
 
-  it("finds lessons by topic", async () => {
-    const topicLessons = await Lesson.find({
-      topic: mongoose.Types.ObjectId("5ba2ef369b1fafcf999a95dc")
+  describe("find", function() {
+    it("finds lessons by topic", async () => {
+      const topicLessons = await Lesson.find({
+        topic: mongoose.Types.ObjectId("5ba2ef369b1fafcf999a95dc")
+      });
+      expect(topicLessons[0]).to.have.property(
+        "name",
+        "Review Diode Current Flow"
+      );
+      expect(topicLessons[0]).to.have.property(
+        "alias",
+        "diode-action-review-diode-current-flow"
+      );
     });
-    expect(topicLessons[0]).to.have.property("name", "Review Diode Current Flow")
-    expect(topicLessons[0]).to.have.property("alias", "diode-action-review-diode-current-flow")
   });
 
-  it("finds one lesson by id", async () => {
-    const topicLessons = await Lesson.find({
-      _id: mongoose.Types.ObjectId("5b5a2cd69b1fafcf999d932e")
+  describe("findOneByIdOrAlias", function() {
+    it("finds one by alias", async () => {
+      const item = await Lesson.findOneByIdOrAlias(
+        "diode-action-review-diode-current-flow"
+      );
+      expect(item).to.have.property("name", "Review Diode Current Flow");
+      expect(item).to.have.property(
+        "alias",
+        "diode-action-review-diode-current-flow"
+      );
     });
-    expect(topicLessons[0]).to.have.property("name", "Review Diode Current Flow")
-    expect(topicLessons[0]).to.have.property("alias", "diode-action-review-diode-current-flow")
+
+    it("finds one by id string", async () => {
+      const item = await Lesson.findOneByIdOrAlias("5bb6540cbecb4e208da0fa6d");
+      expect(item).to.have.property("name", "Polynomials Overview");
+      expect(item).to.have.property(
+        "alias",
+        "polynomials-intro-polynomials-overview"
+      );
+    });
+
+    it("finds one by id object", async () => {
+      const item = await Lesson.findOneByIdOrAlias(
+        mongoose.Types.ObjectId("5bb6540cbecb4e208da0fa6d")
+      );
+      expect(item).to.have.property("name", "Polynomials Overview");
+      expect(item).to.have.property(
+        "alias",
+        "polynomials-intro-polynomials-overview"
+      );
+    });
   });
 });
